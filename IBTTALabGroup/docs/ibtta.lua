0199268e823ede1e37010294e8b355de12ccd1f3
dinkar <dinkar.ganti@gmail.com>
2021-05-08 11:44:31 -0400

    Initial version.

---
+++ IBTTALabGroup/docs/ibtta.lua
@@ -0,0 +1,13 @@
+-- A template file to store any lua code that we need to drive the 
+-- latex files.
+function comma_value(amount)
+  local formatted = amount
+  while true do  
+    formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+    if (k==0) then
+      break
+    end
+  end
+  return formatted
+end
+
---
+++ IBTTALabGroup/scilla-contracts/Notes.md
@@ -0,0 +1,13 @@
+## A general introduction into Zilliqa
+
+### [Scilla](https://www.zilliqa.com/language)
+
+
+### Contracts
+  - TollStateGuarantee
+  - Fleet Account Management
+  - Vehicle management
+
+### Boards
+
+[Fleet Management and IOP](https://milliganpartners.monday.com/)boards/1276485173/)
